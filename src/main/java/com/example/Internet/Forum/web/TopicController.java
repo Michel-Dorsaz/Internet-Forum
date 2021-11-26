a56ca167d8df4add5420156b7c9d9864718971f5
MDorsaz <michel.dorsaz@students.hevs.ch>
2021-11-26 20:00:36 +0200

    RestController

--- src/main/java/com/example/Internet/Forum/web/TopicController.java
+++ src/main/java/com/example/Internet/Forum/web/TopicController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Internet.Forum.domain.Reaction;
 import com.example.Internet.Forum.domain.ReactionRepository;
@@ -31,7 +32,7 @@ import com.example.Internet.Forum.domain.UserRepository;
 import com.example.Internet.Forum.domain.Message;
 import com.example.Internet.Forum.domain.ToolSet;
 
-@Controller
+@RestController
 public class TopicController {
 
 	@Bean
